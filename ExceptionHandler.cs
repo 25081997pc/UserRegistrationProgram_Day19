f11609a8833b9348c7d958cc5a4840f3bf483a76
25081997pc <chavanpooja2508@gmail.com>
2022-06-16 20:57:10 +0530

    Using firstName Method throw exception

---
+++ ExceptionHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class ExceptionHandler
+    {
+        public void firstname()
+        {
+            throw (new FirstNameException("First Name pattern is wrong:First letter should be capital and it should have minimum 3 characters"));
+        }
+    }
+}
