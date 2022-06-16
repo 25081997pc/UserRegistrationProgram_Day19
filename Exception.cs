ce83e6bc88057b0a7e37563cdd37c9f9c17ebc21
25081997pc <chavanpooja2508@gmail.com>
2022-06-16 20:55:31 +0530

    FirstNameException

---
+++ Exception.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class FirstNameException : Exception
+    {
+        public FirstNameException(string messege):base(messege)
+        {
+
+        }
+    }
+}
