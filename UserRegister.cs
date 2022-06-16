90bfce9310f6f6c99fc21edb8f053b82370d09c5
25081997pc <chavanpooja2508@gmail.com>
2022-06-16 22:58:01 +0530

    Password

--- UserRegister.cs
+++ UserRegister.cs
@@ -13,11 +13,10 @@ namespace UserRegistration
         public string regex_LastName = "^[A-Z]{1}[a-z]{2,8}$";
         public string regex_EmailID = "^[a-z0-9]{1,10}[.,+,-]*[a-z0-9]{1,10}[@]{1}[a-z0-9]{1,5}[.]{1}[a-z]{1,3}[.]*[a-z]{1,3}$";
         public string regex_MobileNO = "^[+]*[9]{1}[1]{1}[' ']*[0-9]{10}$";
-        public string regex_Password = "^[A-Z]{1}[a-zA-Z0-9!@#$%^&]{7}$";
+        public string regex_Password = "^[a-z]{8}$";
 
         public bool validateFirstName(string firstname)
         {
-            
             return Regex.IsMatch(firstname, regex_FirstName);
         }
 
@@ -31,7 +30,6 @@ namespace UserRegistration
             return Regex.IsMatch(Email, regex_EmailID);
             
         }
-
         public bool validateMobileNumber(string mobilenum)
         {
             return Regex.IsMatch(mobilenum, regex_MobileNO);
