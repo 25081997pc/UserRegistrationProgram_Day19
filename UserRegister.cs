94508027d08097fc9429566e0472e0b8daea73f9
25081997pc <chavanpooja2508@gmail.com>
2022-06-16 22:35:48 +0530

    ValidateEmail

--- UserRegister.cs
+++ UserRegister.cs
@@ -11,12 +11,13 @@ namespace UserRegistration
     {
         public string regex_FirstName = "^[A-Z]{1}[a-z]{2,8}$";
         public string regex_LastName = "^[A-Z]{1}[a-z]{2,8}$";
-        public string regex_EmailID = "^[a-z0-9]{1,10}[.,+,-]*[a-z0-9]{1,10}[@]{1}[a-z0-9]{1,9}[.]{1}[a-z]{2,5}[.]*[a-z]{1,3}$";
+        public string regex_EmailID = "^[a-z0-9]{1,10}[.,+,-]*[a-z0-9]{1,10}[@]{1}[a-z0-9]{1,5}[.]{1}[a-z]{1,3}[.]*[a-z]{1,3}$";
         public string regex_MobileNO = "^[+]*[9]{1}[1]{1}[' ']*[0-9]{10}$";
         public string regex_Password = "^[A-Z]{1}[a-zA-Z0-9!@#$%^&]{7}$";
 
         public bool validateFirstName(string firstname)
         {
+            
             return Regex.IsMatch(firstname, regex_FirstName);
         }
 
@@ -24,6 +25,7 @@ namespace UserRegistration
         {
             return Regex.IsMatch(lastname, regex_LastName);
         }
+
         public bool validateEmailID(string Email)
         {
             return Regex.IsMatch(Email, regex_EmailID);
